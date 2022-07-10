5c117fca0588eab69eb86879831b1daac225e071
AlexWeissSC <108424707+AlexWeissSC@users.noreply.github.com>
2022-07-10 23:57:19 +0200

    Finished tutorial 8 and 9
    Finished tutorial 8 and 9

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 import copy
+import traceback
 import tcod
+
+import color
 
 from engine import Engine
 import entity_factories
-from input_handlers import EventHandler
 from procgen import generate_dungeon
 
 def main() -> None:
@@ -13,13 +15,14 @@ def main() -> None:
     screen_height = 50
 
     map_width = 80
-    map_height = 45
+    map_height = 43
 
     room_max_size = 10
     room_min_size = 6
     max_rooms = 30
 
     max_monsters_per_room = 2
+    max_items_per_room = 2
 
 
     # The font has 32 chars in a row, and there's a total of 8 rows. Increase the "10" when you add new rows to the sample font file
@@ -27,21 +30,25 @@ def main() -> None:
         "data\dejavu16x16_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
 
-    event_handler = EventHandler()
-
     player = copy.deepcopy(entity_factories.player)
 
-    game_map = generate_dungeon(
+    engine = Engine(player=player)
+
+    engine.game_map = generate_dungeon(
         max_rooms=max_rooms,
         room_min_size=room_min_size,
         room_max_size=room_max_size,
         map_width=map_width,
         map_height=map_height,
         max_monsters_per_room=max_monsters_per_room,
-        player=player
+        max_items_per_room=max_items_per_room,
+        engine=engine,
     )
+    engine.update_fov()
 
-    engine = Engine(event_handler=event_handler, game_map=game_map, player=player)
+    engine.message_log.add_message(
+        "Hello and welcome, adventurer, to yet another dungeon!", color.welcome_text
+    )
 
     with tcod.context.new_terminal(
         screen_width,
@@ -52,12 +59,21 @@ def main() -> None:
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
+            root_console.clear()
+            engine.event_handler.on_render(console=root_console)
+            context.present(root_console)
 
-            engine.render(console=root_console, context=context)
+            try:
+                for event in tcod.event.wait():
+                    context.convert_event(event)
+                    engine.event_handler.handle_events(event)
+            except Exception:  # Handle exceptions in game.
+                traceback.print_exc()  # Print error to stderr.
+                # Then print the error to the message log.
+                engine.message_log.add_message(traceback.format_exc(), color.error)
 
-            events = tcod.event.wait()
 
-            engine.handle_events(events)
+
 
 
 if __name__ == "__main__":
--- procgen.py
+++ procgen.py
@@ -11,7 +11,8 @@ from game_map import GameMap
 import tile_types
 
 if TYPE_CHECKING:
-    from entity import Entity
+    from engine import Engine
+
 class RectangularRoom:
 
     def __init__(self, x: int, y: int, width: int, height: int):
@@ -42,9 +43,10 @@ class RectangularRoom:
             and self.y2 >= other.y1
         )
 def place_entities(
-        room: RectangularRoom, dungeon: GameMap, maximum_monsters: int,
+        room: RectangularRoom, dungeon: GameMap, maximum_monsters: int, maximum_items: int
 ) -> None:
     number_of_monsters = random.randint(0, maximum_monsters)
+    number_of_items = random.randint(0, maximum_items)
 
     for i in range(number_of_monsters):
         x = random.randint(room.x1 + 1, room.x2 - 1)
@@ -55,6 +57,21 @@ def place_entities(
                 entity_factories.rat.spawn(dungeon, x, y)
             else:
                 entity_factories.hound.spawn(dungeon, x, y)
+
+    for i in range(number_of_items):
+        x = random.randint(room.x1 + 1, room.x2 - 1)
+        y = random.randint(room.y1 + 1, room.y2 - 1)
+
+        if not any(entity.x == x and entity.y == y for entity in dungeon.entities):
+            item_chance = random.random()
+            if item_chance < 0.7:
+                entity_factories.health_potion.spawn(dungeon, x, y)
+            elif item_chance < 0.8:
+                entity_factories.fireball_scroll.spawn(dungeon, x, y)
+            elif item_chance < 0.9:
+                entity_factories.confusion_scroll.spawn(dungeon, x, y)
+            else:
+                entity_factories.lightning_scroll.spawn(dungeon, x, y)
 
 def tunnel_between(
     start: Tuple[int, int], end: Tuple[int, int]
@@ -83,10 +100,13 @@ def generate_dungeon(
     map_width: int,
     map_height: int,
     max_monsters_per_room: int,
-    player: Entity,
+    max_items_per_room: int,
+    engine: Engine,
 ) -> GameMap:
     """Generate a new dungeon map."""
-    dungeon = GameMap(map_width, map_height, entities=[player])
+    player = engine.player
+    dungeon = GameMap(engine, map_width, map_height, entities=[player])
+
 
     rooms: List[RectangularRoom] = []
 
@@ -110,13 +130,13 @@ def generate_dungeon(
 
         if len(rooms) == 0:
             # The first room, where the player starts.
-            player.x, player.y = new_room.center
+            player.place(*new_room.center, dungeon)
         else:  # All rooms after the first.
             # Dig out a tunnel between this room and the previous one.
             for x, y in tunnel_between(rooms[-1].center, new_room.center):
                 dungeon.tiles[x, y] = tile_types.floor
 
-        place_entities(new_room, dungeon, max_monsters_per_room)
+        place_entities(new_room, dungeon, max_monsters_per_room, max_items_per_room)
 
         # Finally, append the new room to the list.
         rooms.append(new_room)
